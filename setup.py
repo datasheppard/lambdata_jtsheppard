1bda9fc2a36bf5958a12496b184b8a51dee60406
jtsheppard <jtsheppard@users.noreply.github.com>
2019-09-04 23:28:45 -0400

    Added twine and __init__.py and setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+"""
+lambdata-bjanota - A Collection of Data Science helper functions.
+"""
+import setuptools
+REQUIRED = [
+    "numpy",
+    "pandas"
+]
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+setuptools.setup(
+    name="lambdata-bjanota",
+    version="0.0.1",
+    author="jtsheppard",
+    description="A collection of Data Science helper functions",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jtshepppard/lambdata-jtsheppard",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_required=REQUIRED,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
