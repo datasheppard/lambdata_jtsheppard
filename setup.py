9dd4f4b82482860998c209170cf46c3a47639312
jtsheppard <jtsheppard@users.noreply.github.com>
2019-09-05 00:49:04 -0400

    update to setup.py

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ REQUIRED = [
 with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
 setuptools.setup(
-    name="lambdata-bjanota",
-    version="0.0.2",
-    author="jtsheppard",
+    name="lambdata_jtsheppard",
+    version="0.0.3",
+    author="jtsheppard88",
     description="A collection of Data Science helper functions",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    url="https://github.com/jtshepppard/lambdata-jtsheppard",
+    url="https://github.com/jtshepppard88/lambdata-jtsheppard",
     packages=setuptools.find_packages(),
     python_requires=">=3.5",
     install_required=REQUIRED,
