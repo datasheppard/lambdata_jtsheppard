da01ae712d77662c6e5f0d160cb9079f87edc3c0
jtsheppard <jtsheppard@users.noreply.github.com>
2019-09-08 21:49:13 -0400

    removed oop_lecture

--- oop_lecture.py
+++
@@ -1,9 +0,0 @@
-class Game:
-    """General rep of a game"""
-    def__init__(self, player1='Tina', player2='Raul'):
-    self.round = 3
-    self.player1 = player1
-    self.player2 = player2
-
-    def print_players(self):
-        ""
